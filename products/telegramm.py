285b3a49f3f76f2b664fed24bb980f4a43ee0349
Kirill-34 <kirthorn34@gmail.com>
2023-07-05 19:29:50 +0300

    update requirements.txt

--- products/telegramm.py
+++ products/telegramm.py
@@ -3,7 +3,6 @@ import telepot
 token = '6221081603:AAEpuNdyV_ocSXguJDyAZ4EupSpMxQ4q6ss'
 name = 'Autoparts'
 username = 'AutopartsOilery_bot'
-my_id = 1084912803
 telegramBot = telepot.Bot(token)
 
 def send_message(text):
